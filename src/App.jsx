b85c7224fe9e556703a74f773bfca1c392958d51
Jeremy Helsel <JIHlucky7@gmail.com>
2023-06-29 14:56:18 -0500

    Started ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import OutputBar from './Components/OutputBar'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Password Generator</h1>
-    </>
+    <div className='w-11/12 md:w-1/2'>
+      <h1 className='text-neutral-100'>Password Generator</h1>
+      <OutputBar />
+    </div>
   )
 }
 
---
+++ src/Components/OutputBar.jsx
@@ -0,0 +1,9 @@
+function OutputBar() {
+  return (
+    <div className="bg-neutral-900 || rounded">
+      <h2 className="text-neutral-600">P4$5W0rD!</h2>
+    </div>
+  )
+}
+
+export default OutputBar
